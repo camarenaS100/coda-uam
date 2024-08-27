92116e0ee111aa73bc98c36d72f745ac4fe9d7d2
Mario <mebr1200@gmail.com>
2024-08-26 23:42:45 -0600

    motivo de tutoría muestra etiqueta completa al exportar datos

--- coda-src/Tutorias/admin.py
+++ coda-src/Tutorias/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from.models import Tutoria
-from import_export import resources
+from import_export import resources, fields
 from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import CharWidget
 
 # Register your models here.
 
 class TutoriaResource(resources.ModelResource):
+    tema_display = fields.Field(column_name='tema', attribute='tema', widget=CharWidget())
+
     class Meta:
         model = Tutoria
-        fields = ('id', 'tema', 'alumno', 'tutor', 'descripcion', 'fecha')
+        fields = ('id', 'tema_display', 'alumno', 'tutor', 'descripcion', 'fecha')
+    
+    def dehydrate_tema_display(self, tutoria):
+        return tutoria.get_tema_display()
 
 
 class TutoriasAdmin(ImportExportModelAdmin, admin.ModelAdmin):
