67222791d9206a4b1ee48ec9d36c4b0c0d740024
Mario Baltazar <mebr1200@gmail.com>
2024-08-26 23:18:23 -0600

    test1

--- coda-src/Tutorias/admin.py
+++ coda-src/Tutorias/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from.models import Tutoria
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
-class TutoriasAdmin(admin.ModelAdmin):
-    """Define admin model for Tutorias objects"""
+class TutoriaResource(resources.ModelResource):
+    class Meta:
+        model = Tutoria
+        fields = ('id', 'tema', 'alumno', 'tutor', 'descripcion', 'fecha')
 
-    fieldsets = (
 
-    )
+class TutoriasAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    """Define admin model for Tutoria objects"""
+
+    resource_class = TutoriaResource
+
     list_display = ('tema', 'alumno', 'tutor', 'descripcion', 'fecha')
     search_fields = ('tema', 'alumno', 'tutor', 'fecha')
 
