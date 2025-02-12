f0292acec8cec1b2a63261565420158982bce5e7
Mario Baltazar <mebr1200@gmail.com>
2025-02-12 16:13:29 -0600

    users can now have more than one role, a user who is registered with the 'coordinador' role through the admin panel can also be registered with the 'tutor' role, users need to pick their role manually at the login form

--- coda-src/Usuarios/mixins.py
+++ coda-src/Usuarios/mixins.py
@@ -7,15 +7,17 @@ class ContextConRolesMixin:
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        user_rol = self.request.user.get_rol()
-        if user_rol == ALUMNO:
+        user_roles = self.request.user.get_roles()  # Get list of roles
+
+        if ALUMNO in user_roles:
             context["header_footer"] = TEMPLATES[ALUMNO]
-        elif user_rol == TUTOR:
+        elif TUTOR in user_roles:
             context["header_footer"] = TEMPLATES[TUTOR]
-        elif user_rol == COORDINADOR:
+        elif COORDINADOR in user_roles:
             context["header_footer"] = TEMPLATES[COORDINADOR]
-        elif user_rol == CODA:
+        elif CODA in user_roles:
             context["header_footer"] = TEMPLATES[CODA]
+
         print(context)
         return context
 
@@ -28,17 +30,13 @@ class ContextNotificationsMixin:
         unread_notifications = []
 
         for notification in notifications_raw:
-            notificacion_temp = {}
-            notificacion_temp["header"] =  "Notificacion" if not notification.description else f"{notification.description}"
-            notificacion_temp["text"] = f'{notification.verb} por {Usuario.objects.get(matricula=notification.actor).get_full_name()}'
-            
-            notificacion_temp["time"] = notification.timestamp
-            #remitente = 
-            #notificacion_temp["origen"] = f'{}'
+            notificacion_temp = {
+                "header": "Notificacion" if not notification.description else f"{notification.description}",
+                "text": f'{notification.verb} por {Usuario.objects.get(matricula=notification.actor).get_full_name()}',
+                "time": notification.timestamp
+            }
             unread_notifications.append(notificacion_temp)
-            
 
-       
         context["notificaciones_list"] = unread_notifications
         return context
     
@@ -46,39 +44,35 @@ class BaseAccessMixin(LoginRequiredMixin, ContextConRolesMixin, ContextNotificat
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         return super().get_context_data(**kwargs)
-    
+
 class CodaViewMixin(BaseAccessMixin, UserPassesTestMixin):
 
-
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         return super().get_context_data(**kwargs)
-    
+
     def test_func(self) -> bool:
-        return self.request.user.get_rol()==CODA
-    
+        return self.request.user.has_role(CODA)
+
 class TutorViewMixin(BaseAccessMixin, UserPassesTestMixin):
 
-
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         return super().get_context_data(**kwargs)
-    
+
     def test_func(self) -> bool:
-        return self.request.user.get_rol()==TUTOR
-    
+        return self.request.user.has_role(TUTOR)
+
 class AlumnoViewMixin(BaseAccessMixin, UserPassesTestMixin):
 
-
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         return super().get_context_data(**kwargs)
-    
+
     def test_func(self) -> bool:
-        return self.request.user.get_rol()==ALUMNO
-    
+        return self.request.user.has_role(ALUMNO)
+
 class CordinadorViewMixin(BaseAccessMixin, UserPassesTestMixin):
 
-
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         return super().get_context_data(**kwargs)
-    
+
     def test_func(self) -> bool:
-        return self.request.user.get_rol()==COORDINADOR
+        return self.request.user.has_role(COORDINADOR)
